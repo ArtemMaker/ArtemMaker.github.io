7b8b1dacdeefc1064d68a2b74f671d08691a5805
ArtemMaker <beber4975@gmail.com>
2025-03-02 14:32:02 +0300

    Add files via upload

--- Endless/offline.js
+++ Endless/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1740837952,
+	"version": 1740915054,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -7,6 +7,8 @@
 		"offlineClient.js",
 		"images/line-sheet0.png",
 		"images/box-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/background2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
